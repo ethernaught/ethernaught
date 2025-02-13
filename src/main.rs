9ac3c9f997c8ad82d0fa14a2be6c4122e1c0d7a4
DrBrad <brad@bradeagle.com>
2025-02-12 19:49:17 -0700

    Side panel styling

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::thread;
 use std::time::Duration;
 use ::pcap::{Capture, Device};
 use gtk::prelude::*;
-use gtk::{Application, Builder, gio, CssProvider, StyleContext, gdk, ApplicationWindow, ListBox, ListBoxRow, Label, Orientation, ScrolledWindow, Image, ProgressBar, TreeView, ListStore, CellRendererText, TreeViewColumn, HeaderBar, Toolbar, Button, glib, StackSwitcher, Stack, Paned, TextView, TextBuffer, Adjustment};
+use gtk::{Application, Builder, gio, CssProvider, StyleContext, gdk, ApplicationWindow, ListBox, ListBoxRow, Label, Orientation, ScrolledWindow, Image, ProgressBar, TreeView, ListStore, CellRendererText, TreeViewColumn, HeaderBar, Toolbar, Button, glib, StackSwitcher, Stack, Paned, TextView, TextBuffer, Adjustment, Grid};
 use gtk::gdk::{EventButton, EventMask};
 use gtk::gio::spawn_blocking;
 use gtk::glib::ControlFlow::Continue;
@@ -95,11 +95,21 @@ fn main() {
         stack.add_titled(&window_layout, "application_fragment", "Application");
         stack.set_visible_child_name("application_fragment");
 
-        //window.add(&window_layout);
+        let content_layout: gtk::Box = builder
+            .object("content_layout")
+            .expect("Couldn't find 'content_layout' in window.ui");
+        window_layout.set_child_shrink(&content_layout, false);
+
+        let sidebar_layout: Paned = builder
+            .object("sidebar_layout")
+            .expect("Couldn't find 'sidebar_layout' in window.ui");
+        window_layout.set_child_shrink(&sidebar_layout, false);
 
 
-
-
+        let hex_scroll_layout: ScrolledWindow = builder
+            .object("hex_scroll_layout")
+            .expect("Couldn't find 'hex_scroll_layout' in window.ui");
+        sidebar_layout.set_child_shrink(&hex_scroll_layout, false);
 
 
 
