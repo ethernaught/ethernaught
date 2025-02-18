c4714498e10105f7b7699c5d6b3a7fcccd9ec38c
DrBrad <brad@bradeagle.com>
2025-02-18 11:02:39 -0700

    TreeView swap

--- src/ui/adapters/packet_adapter.rs
+++ src/ui/adapters/packet_adapter.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
-use gtk::{Builder, Label, ListBox, ListBoxRow};
-use gtk::prelude::{BuilderExtManual, ContainerExt, LabelExt, StyleContextExt, WidgetExt};
+use gtk::{Builder, Label, ListBox, ListBoxRow, ListStore};
+use gtk::prelude::{BuilderExtManual, ContainerExt, GtkListStoreExtManual, LabelExt, StyleContextExt, WidgetExt};
 use pcap::packet::inter::interfaces::Interfaces;
 use pcap::packet::layers::layer_1::ethernet_layer::EthernetLayer;
 use pcap::packet::layers::layer_1::inter::types::Types;
@@ -10,60 +10,21 @@ use pcap::packet::packet::Packet;
 
 #[derive(Clone)]
 pub struct PacketAdapter {
-    list_box: ListBox,
+    model: ListStore,
     packets: Arc<Mutex<Vec<Packet>>>
 }
 
 impl PacketAdapter {
 
-    pub fn new(list_box: &ListBox) -> Self {
+    pub fn new(model: &ListStore) -> Self {
         Self {
-            list_box: list_box.clone(),
+            model: model.clone(),
             packets: Arc::new(Mutex::new(Vec::new()))
         }
     }
 
     pub fn add(&mut self, packet: Packet) {
-        let builder = Builder::from_file("res/ui/packet_list_item.xml");
-        let row: ListBoxRow = builder
-            .object("row")
-            .expect("Couldn't find 'row' in packet_list_item.xml");
-
-        //row.style_context().add_class(&packet.get_type().to_string());
-
-        let number_label: Label = builder
-            .object("number")
-            .expect("Couldn't find 'number' in packet_list_item.xml");
-        number_label.set_label(format!("{}", self.packets.lock().as_ref().unwrap().len()+1).as_str());
-
-        let time_label: Label = builder
-            .object("time")
-            .expect("Couldn't find 'time' in packet_list_item.xml");
-        time_label.set_label(format!("{:.5}", packet.get_frame_time()).as_str());
-
-        let source_label: Label = builder
-            .object("source")
-            .expect("Couldn't find 'source' in packet_list_item.xml");
-
-        let destination_label: Label = builder
-            .object("destination")
-            .expect("Couldn't find 'destination' in packet_list_item.xml");
-
-        let protocol_label: Label = builder
-            .object("protocol")
-            .expect("Couldn't find 'protocol' in packet_list_item.xml");
-        //protocol_label.set_label(&packet.get_type().to_string());
-
-        let length_label: Label = builder
-            .object("length")
-            .expect("Couldn't find 'length' in packet_list_item.xml");
-        length_label.set_label(format!("{}", packet.len()).as_str());
-
-        let info_label: Label = builder
-            .object("info")
-            .expect("Couldn't find 'info' in packet_list_item.xml");
-
-        let protocol = match packet.get_interface() {
+        let (source, destination, protocol) = match packet.get_interface() {
             Interfaces::Ethernet => {
                 let ethernet_layer = packet.get_layer(0).unwrap().as_any().downcast_ref::<EthernetLayer>().unwrap();
 
@@ -71,51 +32,54 @@ impl PacketAdapter {
                     Types::IPv4 => {
                         let ipv4_layer = packet.get_layer(1).unwrap().as_any().downcast_ref::<IPv4Layer>().unwrap();
 
-                        source_label.set_label(&ipv4_layer.get_source_ip().to_string());
-                        destination_label.set_label(&ipv4_layer.get_destination_ip().to_string());
+                        //source.set_label(&ipv4_layer.get_source_ip().to_string());
+                        //destination_label.set_label(&ipv4_layer.get_destination_ip().to_string());
 
-                        ipv4_layer.get_protocol().to_string()
-                    }
-                    Types::Arp => {
-                        source_label.set_label(&ethernet_layer.get_source().to_string());
-                        destination_label.set_label(&ethernet_layer.get_destination().to_string());
-                        ethernet_layer.get_type().to_string()
+                        (ipv4_layer.get_source_ip().to_string(), ipv4_layer.get_destination_ip().to_string(), ipv4_layer.get_protocol().to_string())
                     }
                     Types::IPv6 => {
                         let ipv6_layer = packet.get_layer(1).unwrap().as_any().downcast_ref::<IPv6Layer>().unwrap();
 
-                        source_label.set_label(&ipv6_layer.get_source_ip().to_string());
-                        destination_label.set_label(&ipv6_layer.get_destination_ip().to_string());
+                        //source_label.set_label(&ipv6_layer.get_source_ip().to_string());
+                        //destination_label.set_label(&ipv6_layer.get_destination_ip().to_string());
 
-                        ipv6_layer.get_next_header().to_string()
+                        (ipv6_layer.get_source_ip().to_string(), ipv6_layer.get_destination_ip().to_string(), ipv6_layer.get_next_header().to_string())
                     }
                     Types::Broadcast => {
-                        source_label.set_label(&ethernet_layer.get_source().to_string());
-                        destination_label.set_label(&ethernet_layer.get_destination().to_string());
-                        ethernet_layer.get_type().to_string()
+                        //source_label.set_label(&ethernet_layer.get_source().to_string());
+                        //destination_label.set_label(&ethernet_layer.get_destination().to_string());
+                        (ethernet_layer.get_source().to_string(), ethernet_layer.get_destination().to_string(), ethernet_layer.get_type().to_string())
                     }
                     _ => {
-                        ethernet_layer.get_type().to_string()
+                        (ethernet_layer.get_source().to_string(), ethernet_layer.get_destination().to_string(), ethernet_layer.get_type().to_string())
                     }
                 }
 
             }
             Interfaces::WiFi => {
-                "[WiFi] TODO".to_string()
+                //"[WiFi] TODO".to_string()
+                todo!()
             }
             Interfaces::Bluetooth => {
-                "[Bluetooth] TODO".to_string()
+                //"[Bluetooth] TODO".to_string()
+                todo!()
             }
         };
 
-        row.style_context().add_class(&protocol);
-        protocol_label.set_label(&protocol);
+        self.model.insert_with_values(
+            None,
+            &[
+                (0, &(1+self.packets.lock().as_ref().unwrap().len() as u32)),
+                (1, &packet.get_frame_time().to_string()),
+                (2, &source),
+                (3, &destination),
+                (4, &protocol),
+                (5, &packet.len()),
+                (6, &"TODO".to_string())
+            ],
+        );
 
         self.packets.lock().as_mut().unwrap().push(packet);
-
-        row.show_all();
-
-        self.list_box.add(&row);
     }
 
     pub fn get_packet_by_index(&self, index: usize) -> Packet {
--- src/ui/fragment/main_fragment.rs
+++ src/ui/fragment/main_fragment.rs
@@ -1,6 +1,8 @@
 use std::any::Any;
 use gtk::prelude::*;
-use gtk::{gdk, glib, Adjustment, Application, ApplicationWindow, Builder, Button, Container, CssProvider, Image, Label, ListBox, ListBoxRow, Paned, ScrolledWindow, Stack, StyleContext, TextTag, TextView, Widget};
+use gtk::{gdk, glib, Adjustment, Application, ApplicationWindow, Builder, Button, CellRendererText, Container, CssProvider, Image, Label, ListBox, ListBoxRow, ListStore, Paned, ScrolledWindow, Stack, StyleContext, TextTag, TextView, TreePath, TreeView, TreeViewColumn, Widget};
+use gtk::glib::Propagation::Proceed;
+use gtk::glib::Type;
 use pcap::packet::inter::interfaces::Interfaces;
 use pcap::packet::packet::{decode_packet, Packet};
 use crate::ui::activity::inter::activity::Activity;
@@ -29,6 +31,47 @@ impl MainFragment {
     pub fn get_packet_adapter(&self) -> Option<PacketAdapter> {
         self.packet_adapter.clone()
     }
+
+    fn add_column(&self, tree: &TreeView, title: &str, col_id: i32, min_width: i32) {
+        let renderer = CellRendererText::new();
+        let column = TreeViewColumn::new();
+        column.set_min_width(min_width);
+        column.set_title(title);
+        CellLayoutExt::pack_start(&column, &renderer, true);
+        CellLayoutExt::add_attribute(&column, &renderer, "text", col_id);
+
+        CellLayoutExt::set_cell_data_func(&column, &renderer, Some(Box::new(move |_, cell, model, iter| {
+            let protocol: String = model.value(iter, 4).get().unwrap_or_default();
+
+            let color = match protocol.as_str() {
+                "ARP" => {
+                    "#05211b"
+                }
+                "Broadcast" => {
+                    "#000000"
+                }
+                "TCP" => {
+                    "#1e0926"
+                }
+                "UDP" => {
+                    "#070c1f"
+                }
+                "ICMP" => {
+                    "#260d07"
+                }
+                "GRE" => {
+                    "#122407"
+                }
+                _ => {
+                    "#1e1f22"
+                }
+            };
+
+            cell.set_property("cell-background", &color);
+        })));
+
+        tree.append_column(&column);
+    }
 }
 
 impl Fragment for MainFragment {
@@ -40,47 +83,53 @@ impl Fragment for MainFragment {
             .object("content_layout")
             .expect("Couldn't find 'content_layout' in window.ui"));
 
-        let hadjustment = Adjustment::new(0.0, 0.0, 1000.0, 10.0, 100.0, 100.0);
-        let vadjustment = Adjustment::new(0.0, 0.0, 1000.0, 10.0, 100.0, 100.0);
+        let model = ListStore::new(&[Type::U32, Type::STRING, Type::STRING, Type::STRING, Type::STRING, Type::STRING, Type::STRING]);
+        self.packet_adapter = Some(PacketAdapter::new(&model));
 
-        let list_header_scroll_layout: ScrolledWindow = builder
-            .object("list_header_scroll_layout")
-            .expect("Couldn't find 'list_header_scroll_layout' in window.ui");
-        list_header_scroll_layout.set_hadjustment(Some(&hadjustment));
-        list_header_scroll_layout.set_vadjustment(None::<&Adjustment>);
+        let tree_view: TreeView = builder
+            .object("tree_view")
+            .expect("Couldn't find 'tree_view' in window.ui");
 
-        let list_scroll_layout: ScrolledWindow = builder
-            .object("list_scroll_layout")
-            .expect("Couldn't find 'list_scroll_layout' in window.ui");
+        tree_view.set_model(Some(&model));
 
-        list_scroll_layout.set_hadjustment(Some(&hadjustment));
-        list_scroll_layout.set_vadjustment(Some(&vadjustment));
-
-        let list_box: ListBox = builder
-            .object("list_box")
-            .expect("Couldn't find 'list_box' in window.ui");
-
-        self.packet_adapter = Some(PacketAdapter::new(&list_box));
+        self.add_column(&tree_view, "No.", 0, 100);
+        self.add_column(&tree_view, "Time", 1, 150);
+        self.add_column(&tree_view, "Source", 2, 150);
+        self.add_column(&tree_view, "Destination", 3, 150);
+        self.add_column(&tree_view, "Protocol", 4, 80);
+        self.add_column(&tree_view, "Length", 5, 80);
+        self.add_column(&tree_view, "Info", 6, 80);
 
         let _self = self.clone();
-        list_box.connect_row_activated(move |_, row| {
-            let main_activity = _self.activity.as_any().downcast_ref::<MainActivity>().unwrap();
 
-            let packet = _self.packet_adapter.as_ref().unwrap().get_packet_by_index(row.index() as usize);
+        tree_view.connect_button_press_event(move |tree_view, event| {
+            if event.button() == 1 {
+                let (x, y) = event.position();
 
-            let mut sidebar_fragment = SidebarFragment::new(_self.activity.dyn_clone(), packet);
-            main_activity.open_sidebar(sidebar_fragment.dyn_clone());
+                let path = tree_view.path_at_pos(x as i32, y as i32);
+
+                if let Some((Some(path), _column, _x, _y)) = path {
+                    let model = tree_view.model().unwrap();
+                    let id: u32 = model.value(&model.iter(&path).unwrap(), 0).get::<u32>().unwrap()-1;
+
+                    let main_activity = _self.activity.as_any().downcast_ref::<MainActivity>().unwrap();
+
+                    let packet = _self.packet_adapter.as_ref().unwrap().get_packet_by_index(id as usize);
+
+                    let mut sidebar_fragment = SidebarFragment::new(_self.activity.dyn_clone(), packet);
+                    main_activity.open_sidebar(sidebar_fragment.dyn_clone());
+                }
+            }
+
+            Proceed
         });
-
-
-
 
 
         //TEMPORARY
 
         /*
         let hex_data: Vec<u8> = vec![0xe6, 0x38, 0x83, 0x2e, 0xf3, 0x2, 0xf0, 0x77, 0xc3, 0xbe, 0xd0, 0x70, 0x8, 0x0, 0x45, 0x0, 0x0, 0x48, 0x10, 0x1c, 0x0, 0x0, 0x40, 0x11, 0x3d, 0xf8, 0xa, 0x1, 0xc, 0x8f, 0xa, 0x1, 0xc, 0x1, 0x81, 0xf9, 0x0, 0x35, 0x0, 0x34, 0x2c, 0xd7, 0x39, 0xe9, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x3, 0x73, 0x73, 0x6c, 0x7, 0x67, 0x73, 0x74, 0x61, 0x74, 0x69, 0x63, 0x3, 0x63, 0x6f, 0x6d, 0x0, 0x0, 0x41, 0x0, 0x1, 0x0, 0x0, 0x29, 0x5, 0xc0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0];
-        
+
         let packet = decode_packet(Interfaces::Ethernet, &hex_data);
         let main_activity = self.activity.as_any().downcast_ref::<MainActivity>().unwrap();
         let mut sidebar_fragment = SidebarFragment::new(self.activity.dyn_clone(), packet);
