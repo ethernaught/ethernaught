c555b73995de3730563441e29b5a8336ad04350f
DrBrad <brad@bradeagle.com>
2025-03-02 15:36:54 -0700

    DB Fixes

--- src/database/sqlite.rs
+++ src/database/sqlite.rs
@@ -1,6 +1,7 @@
 use core::ptr;
 use core::ffi::c_void;
 use core::ffi::CStr;
+use std::collections::HashMap;
 use std::ffi::CString;
 
 pub struct Database {
@@ -29,22 +30,48 @@ impl Database {
         })
     }
 
-    pub fn insert(&mut self, name: &str, age: i32) {
-        let sql = format!("INSERT INTO users (name, age) VALUES ('{}', {});", name, age);
+    pub fn insert(&mut self, table: &str, fields: &HashMap<&str, String>) {
+        let field_names: Vec<&str> = fields.keys().cloned().collect();
+        let field_values: Vec<String> = fields.values().cloned().collect();
+
+        let field_names_str = field_names.join(", ");
+        let field_values_str = field_values.join(", ");
+
+        let sql = format!(
+            "INSERT INTO {} ({}) VALUES ({});",
+            table,
+            field_names_str,
+            field_values_str
+        );
+
         execute_sql(self.db, &sql);
     }
 
-    pub fn get(&self) {
-        let query = CString::new("SELECT id, name, age FROM users;").unwrap();
+    pub fn get(&self, table: &str, fields: Option<Vec<&str>>, condition: Option<&str>) -> Vec<HashMap<String, String>> {
+        let field_names = match fields {
+            Some(f) => f.join(", "),
+            None => "*".to_string()
+        };
+
+        let sql = match condition {
+            Some(cond) => format!("SELECT {} FROM {} WHERE {}; ", field_names, table, cond),
+            None => format!("SELECT {} FROM {}; ", field_names, table),
+        };
+
+        let mut documents = Vec::new();
+        let query_cstr = CString::new(CString::new(sql).unwrap()).unwrap();
+
         unsafe {
             sqlite3_exec(
                 self.db,
-                query.as_ptr(),
+                query_cstr.as_ptr(),
                 Some(query_callback),
-                ptr::null_mut(),
+                &mut documents as *mut Vec<HashMap<String, String>> as *mut c_void, // Pass the reference to the callback
                 ptr::null_mut(),
             );
         }
+
+        documents
     }
 
     pub fn close(&mut self) {
@@ -72,11 +99,21 @@ extern "C" {
     fn sqlite3_close(db: *mut c_void) -> i32;
 }
 
-extern "C" fn query_callback(_arg: *mut c_void, column_count: i32, column_values: *mut *mut i8, _column_names: *mut *mut i8) -> i32 {
+extern "C" fn query_callback(_arg: *mut c_void, column_count: i32, column_values: *mut *mut i8, column_names: *mut *mut i8) -> i32 {
+    let documents: &mut Vec<HashMap<String, String>> = unsafe {
+        &mut *( _arg as *mut Vec<HashMap<String, String>> )
+    };
+
+    let mut document = HashMap::new();
+
     for i in 0..column_count {
+        let column_name = unsafe { CStr::from_ptr(*column_names.offset(i as isize)) };
         let value = unsafe { CStr::from_ptr(*column_values.offset(i as isize)) };
-        print!("{} ", value.to_string_lossy());
+
+        document.insert(column_name.to_string_lossy().into_owned(), value.to_string_lossy().into_owned());
     }
-    println!();
+
+    documents.push(document);
+
     0
 }
