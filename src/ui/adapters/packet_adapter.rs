8b520be366880458b8e3e1d6ef7c99b9d9f519b3
DrBrad <brad@bradeagle.com>
2025-03-03 09:07:31 -0700

    Country codes

--- src/ui/adapters/packet_adapter.rs
+++ src/ui/adapters/packet_adapter.rs
@@ -145,8 +145,116 @@ impl PacketAdapter {
     //TEMPORARY - MOVE THIS OUT LATER
     fn code_to_icon(code: &str) -> Option<Pixbuf> {
         match code {
-            "US" => {
+            "AM" => {
                 Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok()
+            }
+            "AT" => {
+                Pixbuf::from_file("res/images/flags/ic_austria.svg").ok()
+            }
+            "BE" => {
+                Pixbuf::from_file("res/images/flags/ic_belgium.svg").ok()
+            }
+            "BO" => {
+                Pixbuf::from_file("res/images/flags/ic_bolivia.svg").ok()
+            }
+            "BR" => {
+                Pixbuf::from_file("res/images/flags/ic_brazil.svg").ok()
+            }
+            "BG" => {
+                Pixbuf::from_file("res/images/flags/ic_bulgaria.svg").ok()
+            }
+            "CA" => {
+                Pixbuf::from_file("res/images/flags/ic_canada.svg").ok()
+            }
+            "TD" => {
+                Pixbuf::from_file("res/images/flags/ic_chad.svg").ok()
+            }
+            "CN" => {
+                Pixbuf::from_file("res/images/flags/ic_china.svg").ok()
+            }
+            "DK" => {
+                Pixbuf::from_file("res/images/flags/ic_denmark.svg").ok()
+            }
+            "EE" => {
+                Pixbuf::from_file("res/images/flags/ic_estonia.svg").ok()
+            }
+            "FI" => {
+                Pixbuf::from_file("res/images/flags/ic_finland.svg").ok()
+            }
+            "FR" => {
+                Pixbuf::from_file("res/images/flags/ic_france.svg").ok()
+            }
+            "GA" => {
+                Pixbuf::from_file("res/images/flags/ic_gabon.svg").ok()
+            }
+            "GE" => {
+                Pixbuf::from_file("res/images/flags/ic_georgia.svg").ok()
+            }
+            "DE" => {
+                Pixbuf::from_file("res/images/flags/ic_germany.svg").ok()
+            }
+            "GN" => {
+                Pixbuf::from_file("res/images/flags/ic_guinea.svg").ok()
+            }
+            "HU" => {
+                Pixbuf::from_file("res/images/flags/ic_hungary.svg").ok()
+            }
+            "IE" => {
+                Pixbuf::from_file("res/images/flags/ic_ireland.svg").ok()
+            }
+            "IT" => {
+                Pixbuf::from_file("res/images/flags/ic_italy.svg").ok()
+            }
+            "CI" => {
+                Pixbuf::from_file("res/images/flags/ic_ivory_coast.svg").ok()
+            }
+            "JP" => {
+                Pixbuf::from_file("res/images/flags/ic_japan.svg").ok()
+            }
+            "LV" => {
+                Pixbuf::from_file("res/images/flags/ic_latvia.svg").ok()
+            }
+            "LT" => {
+                Pixbuf::from_file("res/images/flags/ic_lithuania.svg").ok()
+            }
+            "LU" => {
+                Pixbuf::from_file("res/images/flags/ic_luxembourg.svg").ok()
+            }
+            "ML" => {
+                Pixbuf::from_file("res/images/flags/ic_mali.svg").ok()
+            }
+            "NL" => {
+                Pixbuf::from_file("res/images/flags/ic_netherlands.svg").ok()
+            }
+            "NG" => {
+                Pixbuf::from_file("res/images/flags/ic_nigeria.svg").ok()
+            }
+            "PA" => {
+                Pixbuf::from_file("res/images/flags/ic_panama.svg").ok()
+            }
+            "PE" => {
+                Pixbuf::from_file("res/images/flags/ic_peru.svg").ok()
+            }
+            "RO" => {
+                Pixbuf::from_file("res/images/flags/ic_romania.svg").ok()
+            }
+            "RU" => {
+                Pixbuf::from_file("res/images/flags/ic_russia.svg").ok()
+            }
+            "SE" => {
+                Pixbuf::from_file("res/images/flags/ic_sweden.svg").ok()
+            }
+            "SZ" => {
+                Pixbuf::from_file("res/images/flags/ic_switzerland.svg").ok()
+            }
+            "UK" => {
+                Pixbuf::from_file("res/images/flags/ic_united_kingdom.svg").ok()
+            }
+            "US" => {
+                Pixbuf::from_file("res/images/flags/ic_united_states.svg").ok()
+            }
+            "YE" => {
+                Pixbuf::from_file("res/images/flags/ic_yemen.svg").ok()
             }
             _ => None
         }
