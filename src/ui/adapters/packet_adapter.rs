cff81dc4196d14fc7c1c1214dec2c890d2ae4720
DrBrad <brad@bradeagle.com>
2025-03-02 17:49:37 -0700

    Icon changes

--- src/ui/adapters/packet_adapter.rs
+++ src/ui/adapters/packet_adapter.rs
@@ -1,7 +1,7 @@
 use std::sync::{Arc, Mutex};
 use gtk::{Builder, Label, ListBox, ListBoxRow, ListStore};
 use gtk::gdk_pixbuf::Pixbuf;
-use gtk::prelude::{BuilderExtManual, ContainerExt, GtkListStoreExt, GtkListStoreExtManual, LabelExt, StyleContextExt, WidgetExt};
+use gtk::prelude::{BuilderExtManual, ContainerExt, GtkListStoreExt, GtkListStoreExtManual, LabelExt, StyleContextExt, ToValue, WidgetExt};
 use pcap::packet::inter::interfaces::Interfaces;
 use pcap::packet::layers::ethernet_frame::ethernet_frame::EthernetFrame;
 use pcap::packet::layers::ethernet_frame::inter::types::Types;
@@ -30,7 +30,7 @@ impl PacketAdapter {
     }
 
     pub fn add(&mut self, packet: Packet) {
-        let (source, destination, protocol) = match packet.get_interface() {
+        let (source_icon, source, destination_icon, destination, protocol) = match packet.get_interface() {
             Interfaces::Ethernet => {
                 let ethernet_frame = packet.get_frame().as_any().downcast_ref::<EthernetFrame>().unwrap();
 
@@ -44,15 +44,15 @@ impl PacketAdapter {
 
                                 match udp_layer.get_payload() {
                                     UdpPayloads::Known(_type, _) => {
-                                        (ipv4_layer.get_source_address().to_string(), ipv4_layer.get_destination_address().to_string(), udp_layer.get_type().to_string())
+                                        (Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv4_layer.get_source_address().to_string(), Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv4_layer.get_destination_address().to_string(), udp_layer.get_type().to_string())
                                     }
                                     _ => {
-                                        (ipv4_layer.get_source_address().to_string(), ipv4_layer.get_destination_address().to_string(), ipv4_layer.get_protocol().to_string())
+                                        (Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv4_layer.get_source_address().to_string(), Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv4_layer.get_destination_address().to_string(), ipv4_layer.get_protocol().to_string())
                                     }
                                 }
                             }
                             _ => {
-                                (ipv4_layer.get_source_address().to_string(), ipv4_layer.get_destination_address().to_string(), ipv4_layer.get_protocol().to_string())
+                                (Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv4_layer.get_source_address().to_string(), Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv4_layer.get_destination_address().to_string(), ipv4_layer.get_protocol().to_string())
                             }
                         }
                     }
@@ -65,25 +65,25 @@ impl PacketAdapter {
 
                                 match udp_layer.get_payload() {
                                     UdpPayloads::Known(_type, _) => {
-                                        (ipv6_layer.get_source_address().to_string(), ipv6_layer.get_destination_address().to_string(), udp_layer.get_type().to_string())
+                                        (Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv6_layer.get_source_address().to_string(), Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv6_layer.get_destination_address().to_string(), udp_layer.get_type().to_string())
                                     }
                                     _ => {
-                                        (ipv6_layer.get_source_address().to_string(), ipv6_layer.get_destination_address().to_string(), ipv6_layer.get_next_header().to_string())
+                                        (Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv6_layer.get_source_address().to_string(), Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv6_layer.get_destination_address().to_string(), ipv6_layer.get_next_header().to_string())
                                     }
                                 }
                             }
                             _ => {
-                                (ipv6_layer.get_source_address().to_string(), ipv6_layer.get_destination_address().to_string(), ipv6_layer.get_next_header().to_string())
+                                (Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv6_layer.get_source_address().to_string(), Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok(), ipv6_layer.get_destination_address().to_string(), ipv6_layer.get_next_header().to_string())
                             }
                         }
                     }
                     Types::Broadcast => {
                         //source_label.set_label(&ethernet_layer.get_source().to_string());
                         //destination_label.set_label(&ethernet_layer.get_destination().to_string());
-                        (ethernet_frame.get_source_mac().to_string(), ethernet_frame.get_destination_mac().to_string(), ethernet_frame.get_type().to_string())
+                        (None, ethernet_frame.get_source_mac().to_string(), None, ethernet_frame.get_destination_mac().to_string(), ethernet_frame.get_type().to_string())
                     }
                     _ => {
-                        (ethernet_frame.get_source_mac().to_string(), ethernet_frame.get_destination_mac().to_string(), ethernet_frame.get_type().to_string())
+                        (None, ethernet_frame.get_source_mac().to_string(), None, ethernet_frame.get_destination_mac().to_string(), ethernet_frame.get_type().to_string())
                     }
                 }
 
@@ -97,22 +97,30 @@ impl PacketAdapter {
                 todo!()
             }
         };
-        let icon1 = Pixbuf::from_file("res/images/flags/ic_amenia.svg").ok();
 
-        self.model.insert_with_values(
-            None,
-            &[
-                (0, &(1+self.packets.lock().as_ref().unwrap().len() as u32)),
-                (1, &packet.get_frame_time().to_string()),
-                (2, icon1.as_ref().unwrap()),
-                (3, &source),
-                (4, icon1.as_ref().unwrap()),
-                (5, &destination),
-                (6, &protocol),
-                (7, &packet.len().to_string()),
-                (8, &"TODO".to_string())
-            ],
-        );
+        let packet_count = 1 + self.packets.lock().as_ref().unwrap().len() as u32;
+        let frame_time = packet.get_frame_time().to_string();
+        let packet_length = packet.len().to_string();
+
+        let mut values: Vec<(u32, &dyn ToValue)> = vec![
+            (0, &packet_count),
+            (1, &frame_time),
+            (3, &source),
+            (5, &destination),
+            (6, &protocol),
+            (7, &packet_length),
+            //(8, &"TODO".to_string()),
+        ];
+
+        if let Some(ref icon) = source_icon {
+            values.push((2, icon));
+        }
+
+        if let Some(ref icon) = destination_icon {
+            values.push((4, icon));
+        }
+
+        self.model.insert_with_values(None, &values);
 
         self.packets.lock().as_mut().unwrap().push(packet);
     }
