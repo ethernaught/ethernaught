3fe94efa6758441067b92576c3be7864ffafbfab
DrBrad <brad@bradeagle.com>
2025-03-11 12:21:10 -0600

    Open with

--- src/ui/application.rs
+++ src/ui/application.rs
@@ -30,6 +30,71 @@ impl OApplication {
 
     pub fn run(&self) {
         let _self = self.clone();
+        self.app.connect_open(move |app, files, _hint| {
+            for file in files {
+                if let Some(path) = file.path() {
+                    println!("{}", path.to_str().unwrap());
+                    HexEditor::static_type();
+                    Terminal::static_type();
+
+                    let resource_data = include_bytes!("../../res/resources.gresources");
+
+                    let resource = Resource::from_data(&Bytes::from(resource_data)).unwrap();
+                    resources_register(&resource);
+
+                    let builder = Builder::from_resource("/com/ethernaut/rust/res/ui/gtk3/window.ui");
+
+                    let provider = CssProvider::new();
+                    provider.load_from_resource("/com/ethernaut/rust/res/ui/gtk3/window.css");
+                    //provider.load_from_path("res/ui/gtk3/window.css").expect("Failed to load CSS file.");
+
+                    StyleContext::add_provider_for_screen(
+                        &gdk::Screen::default().expect("Failed to get default screen."),
+                        &provider,
+                        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+                    );
+
+                    let window: ApplicationWindow = builder
+                        .object("MainWindow")
+                        .expect("Failed to get the 'MainWindow' from window.ui");
+
+                    window.set_application(Some(app));
+                    window.connect_destroy(|_| exit(0));
+                    //window.set_decorated(false);
+                    window.set_border_width(1);
+
+                    #[cfg(profile = "nightly")]
+                    window.style_context().add_class("nightly");
+
+                    #[cfg(profile = "release")]
+                    window.style_context().add_class("release");
+
+                    //window.set_icon_from_file("res/icons/ic_launcher.svg").expect("Failed to load icon");
+
+                    let mut titlebar = TitleBar::new(_self.clone());
+                    window.set_titlebar(Some(titlebar.on_create()));
+
+                    let window_content: gtk::Box = builder
+                        .object("window_content")
+                        .expect("Failed to get the 'window_content' from window.ui");
+
+                    let stack = Stack::new();
+                    window_content.add(&stack);
+                    stack.show();
+
+                    _self.start_activity(Box::new(DevicesActivity::new(_self.clone())));
+
+                    let mut bottombar = BottomBar::new(_self.clone());
+                    window_content.add(bottombar.on_create());
+
+                    _self.init_actions(&window);
+
+                    window.show();
+                }
+            }
+        });
+
+        let _self = self.clone();
         self.app.connect_activate(move |app| {
             HexEditor::static_type();
             Terminal::static_type();
