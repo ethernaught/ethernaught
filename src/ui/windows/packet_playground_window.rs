5ccf22d0ce10962b507a5a16beb608b4876e563a
DrBrad <brad@bradeagle.com>
2025-03-17 01:10:50 -0600

    Windows

--- src/ui/windows/packet_playground_window.rs
+++ src/ui/windows/packet_playground_window.rs
@@ -1,5 +1,5 @@
-use gtk::{Window, WindowType};
-use gtk::prelude::{GtkWindowExt, WidgetExt};
+use gtk::{gdk, ApplicationWindow, Builder, CssProvider, StyleContext, Window, WindowType};
+use gtk::prelude::{BuilderExtManual, CssProviderExt, GtkWindowExt, WidgetExt};
 
 pub struct PacketPlaygroundWindow {
     window: Option<Window>
@@ -14,11 +14,23 @@ impl PacketPlaygroundWindow {
     }
 
     pub fn on_create(&mut self) {
-        let window = Window::new(WindowType::Toplevel);
-        window.set_title("Packet Playground");
-        window.set_default_size(450, 300);
+        let builder = Builder::from_resource("/com/ethernaut/rust/res/ui/gtk3/packet_playground_window.ui");
 
-        window.show_all();
+        let provider = CssProvider::new();
+        provider.load_from_resource("/com/ethernaut/rust/res/ui/gtk3/packet_playground_window.css");
+        //provider.load_from_path("res/ui/gtk3/window.css").expect("Failed to load CSS file.");
+
+        StyleContext::add_provider_for_screen(
+            &gdk::Screen::default().expect("Failed to get default screen."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+
+        let window: Window = builder
+            .object("PacketPlaygroundWindow")
+            .expect("Failed to get the 'PacketPlaygroundWindow' from window.ui");
+
+        window.show();
 
         self.window = Some(window);
     }
