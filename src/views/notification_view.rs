d2d9987f4f93f7f91eecbe9471e789cc64f50daa
DrBrad <brad@bradeagle.com>
2025-04-01 12:35:12 -0600

    Notifications

--- src/views/notification_view.rs
+++ src/views/notification_view.rs
@@ -1,10 +1,13 @@
-use gtk::{AboutDialog, ApplicationWindow, Builder, Image, Application, TreeViewColumn, CellRendererText, ScrolledWindow, Button, ListBoxRow, Label, CssProvider, StyleContext, gdk, Stack, Container, TreeView, Widget, Window, gio, MenuBar, MenuItem, Menu};
+use gtk::{AboutDialog, ApplicationWindow, Builder, Image, Application, TreeViewColumn, CellRendererText, ScrolledWindow, Button, ListBoxRow, Label, CssProvider, StyleContext, gdk, Stack, Container, TreeView, Widget, Window, gio, MenuBar, MenuItem, Menu, Align};
+use gtk::pango::WrapMode;
 use gtk::prelude::*;
 
 #[derive(Clone)]
 pub struct NotificationView {
     pub root: gtk::Box,
-    pub label: Label
+    icon: Image,
+    pub title: Label,
+    pub description: Label
 }
 
 impl NotificationView {
@@ -16,14 +19,26 @@ impl NotificationView {
             .object("root")
             .expect("Couldn't find 'root' in notification_view.ui");
 
-        let label: Label = builder
-            .object("label")
-            .expect("Couldn't find 'label' in notification_view.ui");
-        label.set_text(message);
+        let icon: Image = builder
+            .object("icon")
+            .expect("Couldn't find 'icon' in notification_view.ui");
+
+        let title: Label = builder
+            .object("title")
+            .expect("Couldn't find 'title' in notification_view.ui");
+        title.set_text("Permissions");
+
+        let description: Label = builder
+            .object("description")
+            .expect("Couldn't find 'description' in notification_view.ui");
+        description.set_wrap(true);
+        description.set_text(message);
 
         Self {
             root,
-            label
+            icon,
+            title,
+            description
         }
     }
 }
