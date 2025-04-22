fb0faeaa3066e79e34ad77aedb852ce5b04ab661
DrBrad <brad@bradeagle.com>
2025-04-21 21:03:51 -0600

    Database cleanup

--- src/database/sqlite.rs
+++ src/database/sqlite.rs
@@ -1,19 +1,18 @@
 use core::ptr;
-use core::ffi::c_void;
 use core::ffi::CStr;
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::io;
 
 pub struct Database {
-    db: *mut c_void
+    db: *mut u32
 }
 
 impl Database {
 
     pub fn open(name: &str) -> io::Result<Self> {
         let c_db_name = CString::new(name).unwrap();
-        let mut db: *mut c_void = ptr::null_mut();
+        let mut db: *mut u32 = ptr::null_mut();
         let rc = unsafe { sqlite3_open(c_db_name.as_ptr(), &mut db) };
         if rc != 0 {
             return Err(io::Error::last_os_error());
@@ -71,7 +70,7 @@ impl Database {
                 self.db,
                 query_cstr.as_ptr(),
                 Some(query_callback),
-                &mut documents as *mut Vec<HashMap<String, String>> as *mut c_void, // Pass the reference to the callback
+                &mut documents as *mut Vec<HashMap<String, String>> as *mut u32, // Pass the reference to the callback
                 ptr::null_mut(),
             );
         }
@@ -84,27 +83,27 @@ impl Database {
     }
 }
 
-fn execute_sql(db: *mut c_void, sql: &str) {
+fn execute_sql(db: *mut u32, sql: &str) {
     let c_sql = CString::new(sql).unwrap();
     unsafe { sqlite3_exec(db, c_sql.as_ptr(), None, ptr::null_mut(), ptr::null_mut()) };
 }
 
 #[link(name = "sqlite3")]
 extern "C" {
-    fn sqlite3_open(filename: *const i8, db: *mut *mut c_void) -> i32;
+    fn sqlite3_open(filename: *const i8, db: *mut *mut u32) -> i32;
 
     fn sqlite3_exec(
-        db: *mut c_void,
+        db: *mut u32,
         sql: *const i8,
-        callback: Option<extern "C" fn(*mut c_void, i32, *mut *mut i8, *mut *mut i8) -> i32>,
-        arg: *mut c_void,
+        callback: Option<extern "C" fn(*mut u32, i32, *mut *mut i8, *mut *mut i8) -> i32>,
+        arg: *mut u32,
         errmsg: *mut *mut i8
     ) -> i32;
 
-    fn sqlite3_close(db: *mut c_void) -> i32;
+    fn sqlite3_close(db: *mut u32) -> i32;
 }
 
-extern "C" fn query_callback(_arg: *mut c_void, column_count: i32, column_values: *mut *mut i8, column_names: *mut *mut i8) -> i32 {
+extern "C" fn query_callback(_arg: *mut u32, column_count: i32, column_values: *mut *mut i8, column_names: *mut *mut i8) -> i32 {
     let documents: &mut Vec<HashMap<String, String>> = unsafe {
         &mut *( _arg as *mut Vec<HashMap<String, String>> )
     };
