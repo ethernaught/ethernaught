39c20189155c9ce830dd7eaa739ba780fb2db8f1
DrBrad <brad@bradeagle.com>
2026-03-06 16:21:06 -0700

    Work on HexEditor

--- src/gtk4/widgets/graph.rs
+++ src/gtk4/widgets/graph.rs
@@ -21,6 +21,7 @@ mod imp {
 
     #[glib::object_subclass]
     impl ObjectSubclass for GraphImpl {
+        
         const NAME: &'static str = "Graph";
         type Type = super::Graph;
         type ParentType = Widget;
@@ -33,6 +34,7 @@ mod imp {
     impl ObjectImpl for GraphImpl {}
 
     impl WidgetImpl for GraphImpl {
+
         fn snapshot(&self, snapshot: &Snapshot) {
             let widget = self.obj();
             let width = widget.width() as f32;
@@ -110,6 +112,7 @@ glib::wrapper! {
 }
 
 impl Graph {
+
     pub fn new() -> Self {
         glib::Object::builder::<Graph>().build()
     }
@@ -153,144 +156,8 @@ impl Graph {
 }
 
 impl Default for Graph {
+
     fn default() -> Self {
         Self::new()
     }
 }
-
-
-/*
-use std::cell::RefCell;
-use gtk4::{glib, Buildable, Orientation, Snapshot, StateFlags, Widget};
-use gtk4::glib::property::PropertyGet;
-use gtk4::graphene::Rect;
-use gtk4::prelude::{DisplayExt, ObjectExt, SnapshotExt, StyleContextExt, WidgetExt};
-use gtk4::subclass::prelude::{ObjectImpl, ObjectSubclass, ObjectSubclassExt, ObjectSubclassIsExt, WidgetClassExt, WidgetImpl, WidgetImplExt};
-
-const MIN_WIDTH: i32 = 20;
-const MIN_HEIGHT: i32 = 20;
-
-#[derive(Default)]
-pub struct GraphImpl {
-    points: RefCell<Vec<u32>>
-}
-
-#[glib::object_subclass]
-impl ObjectSubclass for GraphImpl {
-
-    const NAME: &'static str = "Graph";
-    type ParentType = Widget;
-    type Type = Graph;
-
-    fn class_init(class: &mut Self::Class) {
-        class.set_css_name("graph");
-    }
-}
-
-impl ObjectImpl for GraphImpl {}
-
-impl WidgetImpl for GraphImpl {
-
-    fn snapshot(&self, snapshot: &Snapshot) {
-        let widget = self.obj();
-        let width = widget.width() as f32;
-        let height = widget.height() as f32;
-
-        let cr = snapshot.append_cairo(&Rect::new(0.0, 0.0, width, height));
-
-        let widget = self.obj();
-        let style_context = widget.style_context();
-
-        style_context.set_state(StateFlags::NORMAL);
-        let color = style_context.color();
-
-        let padding = style_context.padding();
-
-        if let Some(background) = style_context.lookup_color("background-color") {
-            cr.set_source_rgba(background.red() as f64, background.green() as f64, background.blue() as f64, background.alpha() as f64);
-            cr.paint();
-        }
-
-        let allocation = self.obj().allocation();
-        let width = allocation.width() as f64;
-        let height = allocation.height() as f64;
-
-        cr.set_line_width(1.0);
-
-        cr.set_source_rgba(color.red() as f64, color.green() as f64, color.blue() as f64, color.alpha() as f64);
-
-        let points = self.points.borrow();
-        if points.len() < 2 {
-            cr.move_to(0.0, height);
-            cr.line_to(width, height);
-            cr.stroke().unwrap();
-
-            return;
-        }
-
-        let min = *points.iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap() as f64;
-        let max = *points.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap() as f64;
-
-        let range = if max > min { max - min } else { 1.0 };
-        let distance = 4.0;// * widget.screen().unwrap().resolution() / 96.0;
-
-        cr.move_to(0.0, height - ((points[0] as f64 - min) / range) * height);
-
-        for (i, &point) in points.iter().enumerate() {
-            let x = i as f64 * distance;
-            let y = height - ((point as f64 - min) / range) * height;
-            cr.line_to(x, y);
-        }
-
-        cr.stroke().unwrap();
-    }
-
-    fn measure(&self, orientation: Orientation, for_size: i32) -> (i32, i32, i32, i32) {
-        match orientation {
-            Orientation::Horizontal => (MIN_WIDTH, MIN_WIDTH, -1, -1),
-            Orientation::Vertical => (MIN_HEIGHT, MIN_HEIGHT, -1, -1),
-            _ => unimplemented!()
-        }
-    }
-}
-
-glib::wrapper! {
-    pub struct Graph(ObjectSubclass<GraphImpl>)
-        @extends Widget, @implements Buildable;
-}
-
-impl Graph {
-
-    pub fn new() -> Self {
-        let _self = glib::Object::builder::<Graph>().build();
-        _self
-    }
-
-    pub fn set_points(&self, points: Vec<u32>) {
-        *self.imp().points.borrow_mut() = points;
-        self.queue_draw();
-    }
-
-    pub fn add_point(&self, point: u32) {
-        let allocation = self.allocation();
-        let width = allocation.width();
-        let distance = 4.0;// * self.screen().unwrap().resolution() / 96.0;
-
-        if self.imp().points.borrow().len() >= width as usize / distance as usize {
-            let size_to_remove = self.imp().points.borrow_mut().len() - width as usize / distance as usize;
-            self.imp().points.borrow_mut().drain(0..size_to_remove);
-        }
-
-        self.imp().points.borrow_mut().push(point);
-        self.queue_draw();
-    }
-
-    pub fn get_points(&self) -> Vec<u32> {
-        self.imp().points.borrow().clone()
-    }
-
-    pub fn clear_points(&self) {
-        self.imp().points.borrow_mut().clear();
-    }
-}
-*/
--- src/gtk4/widgets/hex_editor.rs
+++ src/gtk4/widgets/hex_editor.rs
@@ -9,6 +9,242 @@ use gtk4::prelude::{DisplayExt, ObjectExt, SnapshotExt, StyleContextExt, WidgetE
 use gtk4::subclass::prelude::{ObjectImpl, ObjectImplExt, ObjectSubclass, ObjectSubclassExt, ObjectSubclassIsExt, WidgetClassExt, WidgetImpl, WidgetImplExt};
 
 const BYTES_PER_ROW: usize = 16;
+/*
+mod imp {
+    use std::cell::Cell;
+    use gtk4::graphene::Rect;
+    use gtk4::pango::Weight;
+    use super::*;
+    use gtk4::prelude::StyleContextExt;
+    use gtk4::StateFlags;
+    use gtk4::subclass::prelude::ObjectSubclassExt;
+
+    #[derive(Default, glib::Properties)]
+    #[properties(wrapper_type = super::HexEditor)]
+    pub struct HexEditorImpl {
+        pub data: Cell<Vec<u8>>,
+        #[property(name = "cursor", get, set = Self::set_cursor, type = u64, default = 0)]
+        pub cursor: Cell<u64>,
+        pub selection: Cell<(u64, u64)>,
+        #[property(name = "line_number_color", get, set = Self::set_line_number_color, type = RGBA, default = RGBA::new(1.0, 1.0, 1.0, 1.0))]
+        pub line_number_color: Cell<RGBA>,
+        #[property(name = "cursor_color", get, set = Self::set_cursor_color, type = RGBA, default = RGBA::new(1.0, 1.0, 1.0, 1.0))]
+        pub cursor_color: Cell<RGBA>,
+        #[property(name = "selection_color", get, set = Self::set_selection_color, type = RGBA, default = RGBA::new(1.0, 1.0, 1.0, 1.0))]
+        pub selection_color: Cell<RGBA>
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for HexEditorImpl {
+
+        const NAME: &'static str = "HexEditor";
+        type Type = super::HexEditor;
+        type ParentType = Widget;
+
+        fn class_init(class: &mut Self::Class) {
+            class.set_css_name("hexeditor");
+        }
+    }
+
+    impl ObjectImpl for HexEditorImpl {}
+
+    impl WidgetImpl for HexEditorImpl {
+
+        fn snapshot(&self, snapshot: &Snapshot) {
+            let widget = self.obj();
+            let width = widget.width() as f32;
+            let height = widget.height() as f32;
+
+            let cr = snapshot.append_cairo(&Rect::new(0.0, 0.0, width, height));
+
+            let widget = self.obj();
+            let style_context = widget.style_context();
+
+            style_context.set_state(StateFlags::NORMAL);
+            let text_color = style_context.color();
+
+            let padding = style_context.padding();
+
+            if let Some(background) = style_context.lookup_color("background-color") {
+                cr.set_source_rgba(background.red() as f64, background.green() as f64, background.blue() as f64, background.alpha() as f64);
+                cr.paint();
+            }
+
+            let font_desc = widget.pango_context().font_description().unwrap();
+
+            let font_weight = match font_desc.weight() {
+                Weight::Bold => FontWeight::Bold,
+                _ => FontWeight::Normal
+            };
+
+            cr.select_font_face(font_desc.family().unwrap().split(',').next().unwrap().trim(), FontSlant::Normal, font_weight);
+            cr.set_font_size(font_desc.size() as f64 / 1024.0);// * self.get_monitor_dpi() / 96.0);
+
+            let extents = cr.font_extents().unwrap();
+            let char_width = extents.max_x_advance() + 2.0;
+            let row_padding = 2.0;
+            let row_height = extents.ascent() + extents.descent() + row_padding;
+            let hex_offset = padding.left() as f64 + extents.max_x_advance() + char_width * 8.0;
+            let ascii_offset = hex_offset + extents.max_x_advance() + char_width * (BYTES_PER_ROW as f64 * 2.0);
+
+            for (i, &byte) in self.data.borrow().iter().enumerate() {
+                let row = i / BYTES_PER_ROW;
+                let col = i % BYTES_PER_ROW;
+
+                let hex_x = hex_offset + col as f64 * (char_width * 2.0);
+                let y = padding.top() as f64 + (row as f64 * row_height);
+                let ascii_x = ascii_offset + col as f64 * char_width;
+
+                if col == 0 {
+                    let color = match *self.cursor.borrow() {
+                        Some(cursor) => {
+                            if cursor/BYTES_PER_ROW == row {
+                                text_color
+
+                            } else {
+                                *self.line_number_color.borrow()
+                            }
+                        }
+                        None => {
+                            *self.line_number_color.borrow()
+                        }
+                    };
+
+                    cr.set_source_rgba(color.red() as f64, color.green() as f64, color.blue() as f64, color.alpha() as f64);
+                    let line_number = format!("{:08X}", row * BYTES_PER_ROW);
+
+                    for (i, c) in line_number.chars().enumerate() {
+                        cr.move_to(padding.left() as f64 + (i as f64 * char_width), y + extents.ascent() + row_padding);
+                        cr.show_text(&c.to_string());
+                    }
+                }
+
+                match *self.selection.borrow() {
+                    Some((x, x2)) => {
+                        if i >= x && i <= x+x2-1  {
+                            let color = self.selection_color.borrow();
+                            cr.set_source_rgba(color.red() as f64, color.green() as f64, color.blue() as f64, color.alpha() as f64);
+                            cr.rectangle(hex_x - 1.0, y, char_width * 2.0, row_height);
+                            cr.fill().unwrap();
+
+                            cr.rectangle(ascii_x - 1.0, y, char_width - 2.0 + 2.0, row_height);
+                            cr.fill().unwrap();
+                        }
+                    }
+                    None => {}
+                }
+
+                if Some(i) == *self.cursor.borrow() {
+                    let color = self.cursor_color.borrow();
+                    cr.set_source_rgba(color.red() as f64, color.green() as f64, color.blue() as f64, color.alpha() as f64);
+                    cr.rectangle(hex_x, y + 1.0, char_width * 2.0 - 2.0, row_height - 2.0);
+                    cr.stroke().unwrap();
+
+                    cr.rectangle(ascii_x, y + 1.0, char_width - 2.0, row_height - 2.0);
+                    cr.stroke().unwrap();
+                }
+
+                let color = match byte {
+                    0 => {
+                        match *self.selection.borrow() {
+                            Some((x, x2)) => {
+                                if i >= x && i <= x+x2-1  {
+                                    text_color
+                                } else {
+                                    *self.line_number_color.borrow()
+                                }
+                            }
+                            None => *self.line_number_color.borrow()
+                        }
+                    },
+                    _ => text_color
+                };
+
+                cr.set_source_rgba(color.red() as f64, color.green() as f64, color.blue() as f64, color.alpha() as f64);
+
+                let hex = format!("{:02X}", byte);
+
+                for (i, c) in hex.chars().enumerate() {
+                    cr.move_to(hex_x + (i as f64 * char_width), y + extents.ascent() + row_padding);
+                    cr.show_text(&c.to_string());
+                }
+
+                let ascii_char = if byte.is_ascii_HexEditoric() { byte as char } else { '.' };
+                cr.move_to(ascii_x, y + extents.ascent() + row_padding);
+                cr.show_text(&ascii_char.to_string());
+            }
+        }
+
+        fn measure(&self, orientation: Orientation, for_size: i32) -> (i32, i32, i32, i32) {
+            let (width, height) = self.calculate_size();
+            match orientation {
+                Orientation::Horizontal => (width, width, -1, -1),
+                Orientation::Vertical => (height, height, -1, -1),
+                _ => unimplemented!()
+            }
+        }
+    }
+}
+
+glib::wrapper! {
+    pub struct HexEditor(ObjectSubclass<imp::HexEditorImpl>)
+        @extends Widget,
+        @implements Buildable;
+}
+
+impl HexEditor {
+    
+    pub fn new() -> Self {
+        glib::Object::builder::<HexEditor>().build()
+    }
+
+    pub fn set_data(&self, data: Vec<u8>) {
+        *self.imp().data.borrow_mut() = data.to_vec();
+    }
+
+    pub fn set_line_number_color(&self, color: RGBA) {
+        *self.imp().line_number_color.borrow_mut() = color;
+    }
+
+    pub fn get_line_number_color(&self) -> RGBA {
+        self.imp().line_number_color.borrow().clone()
+    }
+
+    pub fn set_cursor_color(&self, color: RGBA) {
+        *self.imp().cursor_color.borrow_mut() = color;
+    }
+
+    pub fn get_cursor_color(&self) -> RGBA {
+        self.imp().cursor_color.borrow().clone()
+    }
+
+    pub fn set_selection_color(&self, color: RGBA) {
+        *self.imp().selection_color.borrow_mut() = color;
+    }
+
+    pub fn get_selection_color(&self) -> RGBA {
+        self.imp().selection_color.borrow().clone()
+    }
+
+    pub fn set_selection(&self, a: usize, b: usize) {
+        *self.imp().selection.borrow_mut() = Some((a, b));
+        self.queue_draw();
+    }
+
+    pub fn get_selection(&self) -> Option<(usize, usize)> {
+        self.imp().selection.borrow().clone()
+    }
+}
+
+impl Default for HexEditor {
+
+    fn default() -> Self {
+        Self::new()
+    }
+}
+*/
+
+
 
 pub struct HexEditorImpl {
     data: RefCell<Vec<u8>>,
